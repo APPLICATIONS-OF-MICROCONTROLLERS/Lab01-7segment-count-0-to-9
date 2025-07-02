4d4e0854011ddde65e82abaf1e2757c7ff6e39b9
Arthtitaya 67030260 <67030260@kmitl.ac.th>
2025-07-02 13:41:42 +0700

    Sketch.ino

---
+++ Sketch.ino
@@ -0,0 +1,32 @@
+const int segmentPins[7] = {4, 16, 5, 17, 0, 18, 19};
+const byte digitPatterns[10][7] = {
+  {1, 1, 1, 1, 1, 1, 0},  // 0
+  {0, 1, 1, 0, 0, 0, 0},  // 1
+  {1, 1, 0, 1, 1, 0, 1},  // 2
+  {1, 1, 1, 1, 0, 0, 1},  // 3
+  {0, 1, 1, 0, 0, 1, 1},  // 4
+  {1, 0, 1, 1, 0, 1, 1},  // 5
+  {1, 0, 1, 1, 1, 1, 1},  // 6
+  {1, 1, 1, 0, 0, 0, 0},  // 7
+  {1, 1, 1, 1, 1, 1, 1},  // 8
+  {1, 1, 1, 1, 0, 1, 1}   // 9
+};
+
+void setup() {
+  for (int i = 0; i < 7; i++) {
+    pinMode(segmentPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int digit = 0; digit < 10; digit++) {
+    displayDigit(digit);
+    delay(1000);
+  }
+}
+
+void displayDigit(int digit) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segmentPins[i], digitPatterns[digit][i]);
+  }
+}
