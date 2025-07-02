6bd38f2c95cd6d1d88f10d3a71464b140c43b830
Poomchana <poomchana007sim@gmail.com>
2025-07-02 21:30:57 +0700

    Create sketch.ino

---
+++ 67030180/sketch.ino
@@ -0,0 +1,119 @@
+int a=2;
+int b=4;
+int c=14;
+int d=12;
+int e=13;
+int f=5;
+int g=18;
+void setup() {
+  pinMode(a, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(g, OUTPUT);
+}
+
+void loop(){
+  // for 0
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,1);
+  delay(1000);
+
+
+//for 1
+digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+  delay(1000);
+
+ //for 2
+ digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,1);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+  delay(1000);
+
+//for 3
+digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,0);
+  delay(1000);
+
+  //for 4
+  digitalWrite(a,1);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(1000);
+
+  //for 5
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(1000);
+
+  //for 6
+  digitalWrite(a,0);
+  digitalWrite(b,1);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(1000);
+
+  //for 7
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,1);
+  digitalWrite(e,1);
+  digitalWrite(f,1);
+  digitalWrite(g,1);
+  delay(1000);
+
+  //for 8
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,0);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(1000);
+
+  //for 9
+  digitalWrite(a,0);
+  digitalWrite(b,0);
+  digitalWrite(c,0);
+  digitalWrite(d,0);
+  digitalWrite(e,1);
+  digitalWrite(f,0);
+  digitalWrite(g,0);
+  delay(1000);
+  }
