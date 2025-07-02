fc439b20163eb1e578e8ff336ad493678190829d
imnotppee <67030142@kmitl.ac.th>
2025-07-02 16:15:53 +0700

    Update sketch.ino

--- 67030142/sketch.ino
+++ 67030142/sketch.ino
@@ -1,1 +1,32 @@
+// Define GPIO pins connected to each segment
+int segmentPins[] = {2, 4, 5, 18, 19, 21, 22};  // A-G
+
+// Segment patterns for 0–9 (common cathode)
+byte digits[10][7] = {
+  {1, 1, 1, 1, 1, 1, 0},  // 0
+  {0, 1, 1, 0, 0, 0, 0},  // 1
+  {1, 1, 0, 1, 1, 0, 1},  // 2
+  {1, 1, 1, 1, 0, 0, 1},  // 3
+  {0, 1, 1, 0, 0, 1, 1},  // 4
+  {1, 0, 1, 1, 0, 1, 1},  // 5
+  {1, 0, 1, 1, 1, 1, 1},  // 6
+  {1, 1, 1, 0, 0, 0, 0},  // 7
+  {1, 1, 1, 1, 1, 1, 1},  // 8
+  {1, 1, 1, 1, 0, 1, 1}   // 9
+};
+
+void setup() {
+  for (int i = 0; i < 7; i++) {
+    pinMode(segmentPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int num = 0; num < 10; num++) {
+    for (int seg = 0; seg < 7; seg++) {
+      digitalWrite(segmentPins[seg], digits[num][seg]);
+    }
+    delay(1000);
+  }
+}
 
