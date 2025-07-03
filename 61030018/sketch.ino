6ef4a2b176dbfffb9979f0f1d3313448fdd59b4e
Zoluatte <53931447+Zoluatte@users.noreply.github.com>
2025-07-03 15:20:19 +0700

    Update sketch.ino

--- 61030018/sketch.ino
+++ 61030018/sketch.ino
@@ -1,1 +1,41 @@
+const int seg_a = 23;
+const int seg_b = 22;
+const int seg_c = 21;
+const int seg_d = 19;
+const int seg_e = 18;
+const int seg_f = 5;
+const int seg_g = 17;
 
+const int segments[] = {seg_a, seg_b, seg_c, seg_d, seg_e, seg_f, seg_g};
+
+const byte digitPatterns[10][7] = {
+  {1, 1, 1, 1, 1, 1, 0},  // 0
+  {0, 1, 1, 0, 0, 0, 0},  // 1
+  {1, 1, 0, 1, 1, 0, 1},  // 2
+  {1, 1, 1, 1, 0, 0, 1},  // 3
+  {0, 1, 1, 0, 0, 1, 1},  // 4
+  {1, 0, 1, 1, 0, 1, 1},  // 5
+  {1, 0, 1, 1, 1, 1, 1},  // 6
+  {1, 1, 1, 0, 0, 0, 0},  // 7
+  {1, 1, 1, 1, 1, 1, 1},  // 8
+  {1, 1, 1, 1, 0, 1, 1}   // 9
+};
+
+void setup() {
+  for (int i = 0; i < 7; i++) {
+    pinMode(segments[i], OUTPUT);
+  }
+}
+
+void displayDigit(int digit) {
+  for (int i = 0; i < 7; i++) {
+    digitalWrite(segments[i], digitPatterns[digit][i]);
+  }
+}
+
+void loop() {
+  for (int i = 0; i <= 9; i++) {
+    displayDigit(i);
+    delay(1000); 
+  }
+}
